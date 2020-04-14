9a1a879ba6410674f7273a5d692761ba15d59ce7
ismailhelali <40539850+ismailhelali@users.noreply.github.com>
2020-04-15 00:22:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 
+
+           let counter = 0;
+
+            function count() {
+                counter++;
+                document.querySelector('#counter').innerHTML = counter;
+                   
+                if (counter % 10 === 0) {
+                    alert(`I feel Love in the air <3  !`);
+                }
+
+            }
+               
+              let counterh = 0;
+
+            function counth() {
+                counterh++;
+                document.querySelector('#counterh').innerHTML = counterh;
+                   
+                    if (counterh % 10 === 0) {
+                    alert(`i love you , don't forget !`);
+                }
+
+            }
+               
+               
+               
