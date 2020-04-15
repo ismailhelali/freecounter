ef2e7d0e2bb1d9abc812dae32f4971fea7bdfeb3
ismailhelali <40539850+ismailhelali@users.noreply.github.com>
2020-04-15 04:40:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -24,5 +24,13 @@
 
             }
             
-           LocalStorage.setItem("counter","counterh")    ;           
+      
+      function mouseOver() {
+      document.getElementById("demo").style.color = "red";
+       }
+
+      function mouseOut() {
+      document.getElementById("demo").style.color = "black";
+      }
+        
                
